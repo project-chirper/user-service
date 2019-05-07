902904eac69fd29196fb79a70e7afadadfb6efd5
MattL019 <matthewlang019@gmail.com>
2019-05-07 21:27:51 +0100

    added isFollowing route

--- src/routes/index.js
+++ src/routes/index.js
@@ -15,11 +15,12 @@ router.get('/', auth({ required: true }), async (req, res) => {
   let user = await User.findById(req.user)
   if(!user) return res.sendStatus(404)
   return res.json(user.toAuthJSON())
-}) 
+})
 
 router.get('/:user_id', auth({ required: false }), loadUser(), async (req, res) => res.json(await req.targetUser.publicData({ viewer: req.user }))) // fetch user's public data
 router.put('/:user_id/follow', auth({ required: true }), loadUser('followerCount'), require('./follow')) // follow a user
 
 router.get('/:user_id/following', loadUser('following'), (req, res) => res.json(req.targetUser.following)) // gets list of users user is following
+router.get('/:user_id/following/:other_user', loadUser('following'), (req, res) => res.json(req.targetUser.following.indexOf(req.params.other_user) >= 0)) // whether target user is following other user
 
 module.exports = router
